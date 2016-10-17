656876f0e38694dd4a327191547102f1d5f46cad
Aleksandr Kislin <true.axel@gmail.com>
2016-10-17 20:31:36 +0700

    Добавил поддержку сонара, скоро оформлю полный гайд

--- Pattern_trying/src/main/java/sbt/kislin/IntegerConverterValue.java
+++ Pattern_trying/src/main/java/sbt/kislin/IntegerConverterValue.java
@@ -7,11 +7,11 @@ import java.util.Objects;
 /**
  * Created by axelk on 13.10.2016.
  */
-public class IntegerConverterValue implements ConverterTo<Integer> {
-    private Map<Class, Converter> converters = new HashMap<>();
-    @Override
-    public <T> T convert(Objects valueFrom) {
-        ConverterTo<Integer> value = converters.get(valueFrom.getClass());
-        return value;
-    }
-}
+//public class IntegerConverterValue implements ConverterTo<Integer> {
+//    private Map<Class, Converter> converters = new HashMap<>();
+//    @Override
+//    public <T> T convert(Objects valueFrom) {
+//        ConverterTo<Integer> value = converters.get(valueFrom.getClass());
+//        return value;
+//    }
+//}
